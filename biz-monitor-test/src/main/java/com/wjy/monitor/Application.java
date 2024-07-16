ac65373656d0124ec913563aa405c0f58fefd880
wangjiayu <wangdequan1126@gmail.com>
2024-07-16 15:29:37 +0800

    feat: test init

---
+++ biz-monitor-test/src/main/java/com/wjy/monitor/Application.java
@@ -0,0 +1,12 @@
+package com.wjy.monitor;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Configurable
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
